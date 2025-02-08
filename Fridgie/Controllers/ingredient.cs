f0887ba60bc650859682f488b8653852eca778e1
Oceanna-1 <oceannahartog@gmail.com>
2025-02-07 21:25:12 -0500

    Added MVC infrastructure and ingredients

---
+++ Fridgie/Controllers/ingredient.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fridgie.Controllers {
+    public class ingredient {
+        [Key]
+        public int IngredientId { get; set; }
+        [Required]
+        [Display(Name ="Name")]
+        public string IngredientName { get; set; } = "";
+        [Display(Name = "Calories (per 100g")]
+        public int CaloricValue { get; set; }
+        [Required]
+        [Display(Name = "Protein (per 100g")]
+        public int ProteinValue { get; set; }
+
+        [Display(Name = "Sodium (per 100g")]
+        public int? SodiumContent { get; set; }
+        [Display(Name = "Manufacturer?")]
+        public string? Manufacturer { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+
+
+
+    }
+}
---
+++ Fridgie/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Fridgie.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
